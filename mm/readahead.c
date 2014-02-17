c191b3cbaebe043a08e57fd18972b88f49a3c35d
cr3pt <cr3pt@sputnik-sat.info>
2014-02-17 19:54:40 +0100

    Revert "make default readahead size a kernel parameter"
    This reverts commit 0bdc4b09182bca1507616ccf6186fd540c49b54f.
    
    	modified:   Documentation/kernel-parameters.txt
    	modified:   arch/arm/configs/cyanogenmod_m4_defconfig
    	modified:   arch/arm/configs/cyanogenmod_u0_defconfig
    	modified:   arch/arm/configs/cyanogenmod_u0_nonfc_defconfig
    	modified:   include/linux/mm.h
    	modified:   mm/readahead.c

--- mm/readahead.c
+++ mm/readahead.c
@@ -33,15 +33,16 @@ static int __init readahead(char *str)
 		return -EINVAL;
 
 	if (bytes) {
-
-		if (bytes < PAGE_CACHE_SIZE)	/* missed 'k'/'m' suffixes? */
+		if (bytes < PAGE_CACHE_SIZE) /* missed 'k'/'m' suffixes? */
 			return -EINVAL;
-		if (bytes > 128 << 20)		/* limit to 128MB */
-			bytes = 128 << 20;
+
+		if (bytes > 256 << 20)       /* limit to 256MB */
+			bytes = 256 << 20;
 	}
 
 	max_readahead_pages = bytes / PAGE_CACHE_SIZE;
 	default_backing_dev_info.ra_pages = max_readahead_pages;
+
 	return 0;
 }
 
